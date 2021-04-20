ea1389d3995a15282f928e4beb64e472ca4acd7a
Kiana Long <kianajade42.kl@gmail.com>
2021-04-20 16:30:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const returnLastTwoDrivers = function (drivers)
  {return drivers.slice(-2);};
 
 const selectingDrivers=[ returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+const createFareMultiplier = function (multiply){
+  return function (value) {
+    return multiply * value;
+  };
+};
